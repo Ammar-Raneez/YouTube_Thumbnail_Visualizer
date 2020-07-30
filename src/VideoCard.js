9227c8fda93a1baec35001cf42c6f908d86c258d
Ammar <--add>
2020-07-30 14:34:01 +0530

    Finished, with comments

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -3,6 +3,7 @@ import './VideoCard.css'
 import Modal from '@material-ui/core/Modal';
 import { TextField, makeStyles, Button } from '@material-ui/core';
 
+//just some modal configurations and styling
 function getModalStyle() {
     const top = 50;
     const left = 50;
@@ -30,16 +31,21 @@ const useStyles = makeStyles((theme) => ({
 function VideoCard({ thumbnailUrl, channelImageUrl, postedDate, isCustom, title, channelName, noOfViews }) {
     const classes = useStyles();
 
+    //currently entered input, being updated dynamically
     const [input, setInput] = useState('');
+//final image url, we have 2 separate states cuz the image would render dynamically cuz there might be an image for that
+//specific url that we are still entering (ie. we wanna enter google, there can be an image at the url goog, so goog renders
+//and then google renders) so to prevent this unnecessary rendering we have two states
     const [imageUrl, setImageUrl] = useState('');
 
+//title can have only one state, but kept the same just because
     const [thumbTitle, setThumbTitle] = useState('');
     const [titleInput, setTitleInput] = useState('');
 
     const [open, setOpen] = useState(false);
     const [modalStyle] = React.useState(getModalStyle);
 
-
+    //the final output is set only upon submitting
     const handleClose = event => {
         event.preventDefault();
         setImageUrl(input);
@@ -71,6 +77,7 @@ function VideoCard({ thumbnailUrl, channelImageUrl, postedDate, isCustom, title,
                 </div>
 
                 <div className="videoCard__infoText">
+{/*if we have added a new thumbnail, thumbTitle is present and so that displays, else the currently displayed one is*/}
                     <p className="videoCard__title">{thumbTitle || title}</p>
                     <p className="videoCard__channelName">{channelName}</p>
                     <p className="videoCard__noOfViewsPostedDate">{noOfViews} • {postedDate}</p>
